443b941acae8dbab0e6ccc95f0307063d29a1d1d
vinaypoosala <52882442+vinaypoosala@users.noreply.github.com>
2019-11-01 18:39:28 +0530

    added templete

--- webproject/models.py
+++ webproject/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(settings.AUTH_USER_MODEL,max_length=20)
+    text = models.TextField()
+    author = models.CharField(max_length=30)
+    created_date = models.DateTimeField(default = timezone.now)
+    published_date = models.DateTimeField(blank=True,null= True)
+
+
+    def publish(self):
+        self.published_date = timezone.now
+        self.save()
+
+    def __str__(self):
+        return self.title    
