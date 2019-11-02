ec4abea4e906b547c1b3c46212a37fbd13b04c47
vinaypoosala <52882442+vinaypoosala@users.noreply.github.com>
2019-11-02 20:13:56 +0530

    all changes were done

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2oo5d0d0*3!k0f(-p0iztka)x9we3g2hf)=9xq(enq-o8ry#hk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['vinaypoosala09.pythonanywhere.com','127.0.0.1','local host']
 
